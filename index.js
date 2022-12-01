2b75eb533124256f49d9eb24472c16900a63ab20
andriGo <afomin129@gmail.com>
2022-12-02 01:41:08 +0200

    master

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function compoundInterest(startDeposit, annualRate, annualAmount, additionalInvestments, interestPeriod) {
-	// write your code here
+	let StrDep = startDeposit;
+
+	for (let i = 0; i < annualAmount * interestPeriod; i++) {
+		StrDep += StrDep * ((annualRate / annualAmount) / 100) + additionalInvestments;
+	}
+
+	return Math.round(StrDep * 100) / 100;
+
 }
 
 module.exports = compoundInterest;
